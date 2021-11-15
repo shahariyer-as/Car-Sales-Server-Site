d9a9aa7a30da86ce792264b0db49ae9be9f65062
shahariyer-as <shahariyer.as@gmail.com>
2021-11-15 12:39:12 +0600

    some portd

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const { query } = require('express');
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 
 
